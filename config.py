22ec9e30fd859d62031bece2ced7f90b396b614d
m6hmd <85321205+m6hmd@users.noreply.github.com>
2022-03-20 03:12:48 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 BOT_TOKEN = "1206956607:AAGApSGAcjNK_eKu8gad72IZgaNtKLV2fiQ"
-APP_URL = "https://telegrambooot.herokuapp.com/" + BOT_TOKEN
+APP_URL = "https://bottttelegram.herokuapp.com/" + BOT_TOKEN
